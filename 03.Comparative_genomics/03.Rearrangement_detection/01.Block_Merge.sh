e7062725fa8c0b9f8e741e903e6c9ba464aa8073
Jing <nwsuaf_lj@163.com>
2020-11-29 12:48:31 +0100

    Create 01.Block_Merge.sh

---
+++ 03.Comparative_genomics/03.Rearrangement_detection/01.Block_Merge.sh
@@ -0,0 +1,3 @@
+perl merge_block.New.R1.pl Sea_turtle.Emu.m8 10000 20000 Sea_turtle.Emu.R1.blocks
+perl merge_block.New.R2.pl Sea_turtle.Emu.R1.blocks Sea_turtle.Emu.R2.blocks
+perl merge_block.New.R2.pl Sea_turtle.Emu.R2.blocks Sea_turtle.Emu.R3.blocks
