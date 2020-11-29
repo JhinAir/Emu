023eb5d95696d130e3fcc679d3a6231e6aeaa3f5
Jing <nwsuaf_lj@163.com>
2020-11-29 12:53:26 +0100

    Create Genewise.sh

---
+++ 05.Sex_chromosome_evolution/02.W_degeneration/Genewise.sh
@@ -0,0 +1,4 @@
+export WISECONFIGDIR=/public/home/lijing/software/wise2.4.1/src/../wisecfg
+/public/home/lijing/software/wise2.4.1/src/bin/genewise -tfor -genesf  -quiet  /public/home/lijing/project/06.fly/01.annotation.pep.v2/Dpse/03.genewise/./all.genewise/01/001/XP_015044051.2-D1.fa /public/home/lijing/project/06.fly/01.annotation.pep.v2/Dpse/03.genewise/./all.genewise/01/001/XP_015044051.2-D1_X_38178718_38184567.fa > /public/home/lijing/project/06.fly/01.annotation.pep.v2/Dpse/03.genewise/./all.genewise/01/001/XP_015044051.2-D1_X_38178718_38184567.genewise
+
+/home/user/liu/Software/genewise/gw_parser.pl --pep XP_015044051.2 --gw XP_015044051.2.genewise.out --ac 0 --id 0 --type 2 > XP_015044051.2.mut
